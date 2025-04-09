a25b3d9e4097df1ac07b6f0f7fcc1282f4ce64ac
Malik <timurkaev_malik@icloud.com>
2025-04-09 13:19:24 +0300

    Initial Commit

---
+++ СryptoСurrency/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  СryptoСurrency
+//
+//  Created by Malik Timurkaev on 09.04.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
---
+++ СryptoСurrency/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  СryptoСurrency
+//
+//  Created by Malik Timurkaev on 09.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+}
+
