750744cffa98ca558158667daa7457d4703198e6
Malik <timurkaev_malik@icloud.com>
2025-04-10 14:19:22 +0300

    добавил loginTextField на AuthViewController

--- СryptoСurrency/Screens/Auth/AuthViewController.swift
+++ СryptoСurrency/Screens/Auth/AuthViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    private lazy var loginTextField = AuthTextField(type: .login)
     private lazy var passwordTextField = AuthTextField(type: .password)
 
     private lazy var loginButton: LoginButton = {
@@ -21,6 +22,7 @@ final class AuthViewController: UIViewController {
         view.backgroundColor = .ypWhiteMedium
         setupLoginButton()
         setupPasswordTextField()
+        setupLoginTextField()
     }
     
     private func setupLoginButton() {
@@ -42,6 +44,17 @@ final class AuthViewController: UIViewController {
             passwordTextField.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -25),
             passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
             passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+        ])
+    }
+    
+    private func setupLoginTextField() {
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginTextField)
+        
+        NSLayoutConstraint.activate([
+            loginTextField.bottomAnchor.constraint(equalTo: passwordTextField.topAnchor, constant: -15),
+            loginTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            loginTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
         ])
     }
     
