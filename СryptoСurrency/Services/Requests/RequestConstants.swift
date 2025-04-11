0c0b6971717ba1c6cd03723524e8f8ace0992531
Malik <timurkaev_malik@icloud.com>
2025-04-11 08:35:23 +0300

    добавил enum-ы для endPoint

---
+++ СryptoСurrency/Services/Requests/RequestConstants.swift
@@ -0,0 +1,13 @@
+//
+//  RequestConstants.swift
+//  Сryptoсurrency
+//
+//  Created by Malik Timurkaev on 11.04.2025.
+//
+
+import Foundation
+
+enum RequestConstants {
+    static let baseURL = "https://data.messari.io/api/v1"
+    static let apiKey = "c0JxcixjmN16fNkjo+JU6QFKqgbvkCtVg1Hu6uJWxE4zRyxM"
+}
