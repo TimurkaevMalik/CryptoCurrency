c845c9c6ca6bf37559059d4facd236738002963c
Malik <timurkaev_malik@icloud.com>
2025-04-11 12:37:05 +0300

    добавил menuButton

---
+++ СryptoСurrency/CustomViews/MenuButton.swift
@@ -0,0 +1,47 @@
+//
+//  MenuButton.swift
+//  Сryptoсurrency
+//
+//  Created by Malik Timurkaev on 11.04.2025.
+//
+
+import UIKit
+
+final class MenuButton: UIButton {
+    
+    private let actions: [UIAction]
+    
+    init(actions: [UIAction]) {
+        self.actions = actions
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .ypWhite.withAlphaComponent(0.8)
+        layer.cornerRadius = 24
+        layer.masksToBounds = true
+        setImage(.ellipsis, for: .normal)
+        let size: CGFloat = 48
+        let anchorView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 60))
+        addSubview(anchorView)
+        
+        menu = UIMenu(children: actions)
+        showsMenuAsPrimaryAction = true
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: size),
+            widthAnchor.constraint(equalToConstant: size)
+        ])
+    }
+    
+    override func menuAttachmentPoint(for configuration: UIContextMenuConfiguration) -> CGPoint {
+        
+        let original = super.menuAttachmentPoint(for: configuration)
+        return CGPoint(x: original.x, y: original.y + 8)
+    }
+}
--- СryptoСurrency/Scenes/HomeViewController/HomeViewController.swift
+++ СryptoСurrency/Scenes/HomeViewController/HomeViewController.swift
@@ -11,12 +11,25 @@ final class HomeViewController: UIViewController {
     
     private lazy var titleView = UILabel()
     
+    private lazy var menuButton: MenuButton = {
+        let logoutAction = UIAction(title: "Выйти", image: .bin) { _ in
+            print("logoutAction")
+        }
+        let updateAction = UIAction(title: "Обновить", image: .rocket) { _ in
+            print("updateAction")
+        }
+        
+        return MenuButton(actions: [logoutAction,
+                                    updateAction])
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .ypPink
         navigationController?.setNavigationBarHidden(true, animated: false)
         
         setupTitleView()
+        setupMenuButton()
     }
     
     private func setupTitleView() {
@@ -30,8 +43,18 @@ final class HomeViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             titleView.heightAnchor.constraint(equalToConstant: 48),
-            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .defaultMargin),
             titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)
+        ])
+    }
+    
+    private func setupMenuButton() {
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(menuButton)
+        
+        NSLayoutConstraint.activate([
+            menuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.defaultMargin),
+            menuButton.centerYAnchor.constraint(equalTo: titleView.centerYAnchor)
         ])
     }
 }
